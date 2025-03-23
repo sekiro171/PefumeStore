8f884db461d014b0e528a088794ff6562ef8f42a
Phongxuan123 <158146102+Phongxuan123@users.noreply.github.com>
2025-03-23 19:03:42 +0700

    Delete java - Copy directory

--- java - Copy/perfumeshopDB/DatabaseInfo.java
+++
@@ -1,8 +0,0 @@
-package perfumeshopDB;
-
-public interface DatabaseInfo {
-    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String DBURL="jdbc:sqlserver://DESKTOP-CIUU9E1\\SQLEXPRESS;databaseName=Perfume_Website;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
-    public static String USERDB="sa";
-    public static String PASSDB="123";
-}
